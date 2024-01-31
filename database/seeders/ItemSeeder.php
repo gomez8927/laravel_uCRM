0ed86a16ecc12f4a7789de53e3a993d58551c5bb
Kyosuke Horigome <horigome@outlook.jp>
2024-01-31 16:18:34 +0900

    商品関連

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class ItemSeeder extends Seeder
 {
@@ -14,6 +16,22 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('items')->insert([
+            [
+            'name' => 'ポケカ',
+            'memo' => 'ポケカの詳細',
+            'price' => 500,
+            ],
+            [
+            'name' => '遊戯王',
+            'memo' => '遊戯王の詳細',
+            'price' => 1000,
+            ],
+            [
+            'name' => 'デュエマ',
+            'memo' => 'デュエマの詳細',
+            'price' => 1500,
+            ],
+        ]);
     }
 }
